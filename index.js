51ea9d6a93ed4e30d2e34a2cb0f60bce1ea11813
Upal Barua <upalbarua52@gmail.com>
2022-10-26 10:24:00 +0600

    Added fake data and setup cors

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const courses = require('./data/courses.json')
 const app = express()
+const cors = require('cors')
 const port = 5000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/courses', (req, res) => {
+  res.json(courses)
 })
 
 app.listen(port)
