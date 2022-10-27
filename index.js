f8c2b42cab34dba84701c7256d66ed70dea68a96
Upal Barua <upalbarua52@gmail.com>
2022-10-27 18:46:18 +0600

    Did some refactoring

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
-app.use(cors());
+const cors = require('cors');
 const courses = require('./data/courses.json');
 const blog = require('./data/blog.json');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(cors());
 
 app.get('/courses', (req, res) => {
   res.json(courses);
+});
+
+app.get('/blog', (req, res) => {
+  res.json(blog);
 });
 
 app.get('/details/:courseId', (req, res) => {
@@ -22,8 +23,24 @@ app.get('/details/:courseId', (req, res) => {
   res.json(courseDetails);
 });
 
-app.get('/blog', (req, res) => {
-  res.json(blog);
+app.get('/featured', (req, res) => {
+  const featuredCourses = courses.filter(course => course.isFeatured);
+
+  res.json(featuredCourses);
 });
 
-app.listen(5000);
+app.get('/bestseller', (req, res) => {
+  const bestsellerCourses = courses.filter(course => course.isBestSeller);
+
+  res.json(bestsellerCourses);
+});
+
+app.get('/details/:courseId', (req, res) => {
+  const courseDetails = courses.find(
+    course => course.id === req.params.courseId
+  );
+
+  res.json(courseDetails);
+});
+
+app.listen(3000);
